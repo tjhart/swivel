7ad85045885a795bce1b4cd5f6502040f97feeff
tjhart <tjhart@me.com>
2013-09-10 10:18:11 -0500

    Abstracted URI resolution

--- swivel-server/src/test/java/com/tjh/swivel/model/DynamicStubRequestHandlerTest.java
+++ swivel-server/src/test/java/com/tjh/swivel/model/DynamicStubRequestHandlerTest.java
@@ -1,6 +1,7 @@
 package com.tjh.swivel.model;
 
 import com.tjh.swivel.utils.ScriptWrapper;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.hamcrest.Matcher;
 import org.junit.Before;
@@ -9,6 +10,7 @@ import org.mockito.ArgumentCaptor;
 
 import javax.script.Bindings;
 import javax.script.ScriptException;
+import java.net.URI;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -36,7 +38,7 @@ public class DynamicStubRequestHandlerTest {
     public void handleDefersToScriptWrapper() throws ScriptException {
         dynamicResponseHandler.scriptWrapper = mock(ScriptWrapper.class);
 
-        dynamicResponseHandler.handle(mockRequest, null, null);
+        dynamicResponseHandler.handle(mockRequest, URI.create("matched/uri"), mock(HttpClient.class));
 
         ArgumentCaptor<Bindings> bindingsCaptor = ArgumentCaptor.forClass(Bindings.class);
 

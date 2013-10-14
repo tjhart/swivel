25cb8247902dbb7d2cf0a184df000e16f32c8fc4
tjhart <tjhart@me.com>
2013-10-14 17:08:13 -0500

    Fixed bug - not all headers should be copied from the original response - need to circle back around later hand handle http headers more appropriately

--- swivel-server/src/main/java/com/tjh/swivel/controller/JerseyResponseFactory.java
+++ swivel-server/src/main/java/com/tjh/swivel/controller/JerseyResponseFactory.java
@@ -4,12 +4,18 @@ import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
+import vanderbilt.util.Sets;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.Set;
 
 public class JerseyResponseFactory {
+    //REDTAG:TJH - another place where proxy behavior and HTTP headers needs to be more fully
+    //researched and implemented
+    public static final Set<String> EXCLUDED_HEADERS = Sets.asConstantSet("Transfer-Encoding");
+
     public Response createResponse(HttpResponse response) throws IOException {
         StatusLine statusLine = response.getStatusLine();
         Response.ResponseBuilder builder =
@@ -20,9 +26,11 @@ public class JerseyResponseFactory {
         }
 
         for (Header header : response.getAllHeaders()) {
-            builder.header(header.getName(), header.getValue());
-            if (header.getName().equals(HttpHeaders.CONTENT_TYPE)) {
-                builder.type(header.getValue());
+            if (!EXCLUDED_HEADERS.contains(header.getName())) {
+                builder.header(header.getName(), header.getValue());
+                if (header.getName().equals(HttpHeaders.CONTENT_TYPE)) {
+                    builder.type(header.getValue());
+                }
             }
         }
 

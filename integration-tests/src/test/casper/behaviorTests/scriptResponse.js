6063dd508c4d7b381eb62c4f30b2c0c7b139b477
tjhart <tjhart@me.com>
2013-10-21 18:27:23 -0500

    Created dynamic response test

---
+++ integration-tests/src/test/casper/behaviorTests/scriptResponse.js
@@ -0,0 +1,33 @@
+(function () {
+    var swivelUtils = require('../lib/swivelUtils');
+    const PROXY_URL = [swivelUtils.PROXY_URL, 'some/path'].join('/');
+
+    swivelUtils.reset(function () {
+        swivelUtils.configureStub('some/path', {
+            description: 'Test: static response',
+            when: {method: 'GET'},
+            then: {script: '(function(request, matchedURI, responseFactory, client){\n' +
+                '   var content = \'requestedURI:\' + request.getURI() +\n' +
+                '       \', matchedURI:\' + matchedURI;\n' +
+                '\n' +
+                '   return responseFactory.createResponse({\n' +
+                '           statusCode:200,\n' +
+                '           contentType:\'text/plain\',\n' +
+                '           content:content});\n' +
+                '})(request, matchedURI, responseFactory, client);'}
+        }, function () {
+            casper.test.begin('Static stub response', function (test) {
+                casper.start(PROXY_URL, function (response) {
+                    test.assertHttpStatus(200, 'status code matches');
+                    test.assertEquals(response.contentType, 'text/plain');
+                    test.assertEquals(casper.page.plainText, 'requestedURI:some/path, matchedURI:some/path',
+                        'content matches');
+                });
+
+                casper.run(function () {
+                    test.done();
+                });
+            });
+        });
+    });
+})();
